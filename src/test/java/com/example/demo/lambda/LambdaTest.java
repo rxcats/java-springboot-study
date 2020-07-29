1127755e2e6f2ac854c0751db8f21af3a1e41c52
rxcats <windpyj80@gmail.com>
2020-07-29 14:44:25 +0900

    lambda test 추가

--- src/test/java/com/example/demo/lambda/LambdaTest.java
+++ src/test/java/com/example/demo/lambda/LambdaTest.java
@@ -1,4 +1,54 @@
 package com.example.demo.lambda;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.lang.NonNull;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class LambdaTest {
+
+    /**
+     * @see Runnable
+     * @see Supplier
+     * @see Consumer
+     * @see Function
+     * @see Predicate
+     */
+
+    // 익명 함수
+    private final Predicate<String> fun = new Predicate<String>() {
+        @Override
+        public boolean test(@NonNull String s) {
+            return s.equals("hello");
+        }
+    };
+
+    // 람다 표현식
+    private final Predicate<String> fun2 = (@NonNull String s) -> s.equals("hello");
+
+    @Test
+    void useOverride() {
+        assertThat(fun.test("hello")).isTrue();
+        assertThat(fun.test("world")).isFalse();
+    }
+
+    @Test
+    void useLambdaExpression() {
+        assertThat(fun2.test("hello")).isTrue();
+        assertThat(fun2.test("world")).isFalse();
+    }
+
+    @Test
+    void nullException() {
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            System.out.println(fun.test(null));
+        });
+    }
+
 }
