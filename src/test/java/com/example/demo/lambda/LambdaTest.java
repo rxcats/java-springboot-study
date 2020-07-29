e13046a5dad2d282634a3045e25178ff8d63e1b1
rxcats <windpyj80@gmail.com>
2020-07-29 16:32:41 +0900

    stream, thread 테스트 추가

--- src/test/java/com/example/demo/lambda/LambdaTest.java
+++ src/test/java/com/example/demo/lambda/LambdaTest.java
@@ -1,5 +1,14 @@
 package com.example.demo.lambda;
 
+import java.lang.annotation.Annotation;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -9,9 +18,39 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.lang.NonNull;
 
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LambdaTest {
+
+    @Target({ ElementType.TYPE})
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface ForA {
+
+    }
+
+    @ForA
+    @ToString
+    @NoArgsConstructor
+    static class A {
+        private String name;
+    }
+
+    @Test
+    void info() {
+        // lombok annotation 은 컴파일 시점에 코드를 생성 하기 때문에 runtime 에 가져올 수 없어서 나오지 않음.
+        for (Annotation annotation : A.class.getAnnotations()) {
+            System.out.println(annotation);
+        }
+
+        System.out.println();
+
+        for (Method method : A.class.getMethods()) {
+            System.out.println(method);
+        }
+    }
 
     /**
      * @see Runnable
@@ -20,6 +59,15 @@ public class LambdaTest {
      * @see Function
      * @see Predicate
      */
+
+    @FunctionalInterface
+    interface R {
+        void doit();
+    }
+
+    private final R r = () -> {
+        System.out.println("r");
+    };
 
     // 익명 함수
     private final Predicate<String> fun = new Predicate<String>() {
@@ -30,7 +78,7 @@ public class LambdaTest {
     };
 
     // 람다 표현식
-    private final Predicate<String> fun2 = (@NonNull String s) -> s.equals("hello");
+    private final Predicate<String> lambdaFun = (@NonNull String s) -> s.equals("hello");
 
     @Test
     void useOverride() {
@@ -40,14 +88,82 @@ public class LambdaTest {
 
     @Test
     void useLambdaExpression() {
-        assertThat(fun2.test("hello")).isTrue();
-        assertThat(fun2.test("world")).isFalse();
+        assertThat(lambdaFun.test("hello")).isTrue();
+        assertThat(lambdaFun.test("world")).isFalse();
     }
 
     @Test
-    void nullException() {
+    void nonNullAnnotation() {
         Assertions.assertThrows(NullPointerException.class, () -> {
             System.out.println(fun.test(null));
+        });
+    }
+
+    @Test
+    void doAsyncWork() throws InterruptedException {
+        System.out.println("start " + Thread.currentThread().getName());
+
+        var job = new Thread(() -> {
+            try {
+                Thread.sleep(3000);
+                System.out.println("hello " + Thread.currentThread().getName());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        job.start();
+
+        System.out.println("end " + Thread.currentThread().getName());
+
+        Thread.sleep(5000);
+
+
+        var pool = Executors.newFixedThreadPool(1);
+
+        try {
+            pool.submit(() -> {
+                System.out.println("ok" + Thread.currentThread().getName());
+            }).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @Test
+    void threadPool() throws InterruptedException {
+
+        var pool = Executors.newFixedThreadPool(1);
+
+        try {
+            pool.submit(() -> {
+                System.out.println("ok" + Thread.currentThread().getName());
+            }).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        Thread.sleep(5000);
+    }
+
+    @Test
+    void stream() {
+        var list = List.of(1, 2, 3, 4, 5);
+
+        list.stream()
+            .filter(i -> i > 2)
+            .map(i -> i * 3)
+            .forEach(System.out::println);
+    }
+
+    @Test
+    void parallelStream() {
+        var list = List.of(1, 2, 3, 4, 5);
+
+        list.parallelStream().forEach(v -> {
+            System.out.println(v + " " + Thread.currentThread().getName());
         });
     }
 
